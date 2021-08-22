22f01e9755db785519fa54101708e3d36ae0538b
yoycode <65293082+yoycode@users.noreply.github.com>
2021-08-22 19:27:35 +0900

    8/22 commit

--- 섹션1/1-15.js
+++ 섹션1/1-15.js
@@ -1,8 +1,15 @@
 function solution(s) {
   let answer;
+  let idx;
 
-  for (let i in s) {
-    console.log(i);
+  for (let x of s) {
+    if (s.length % 2 == 0) {
+      idx = Math.floor(s.length / 2)
+      answer = s[idx - 1] + s[idx]
+    } else {
+      idx = Math.ceil(s.length / 2)
+      answer = s[idx - 1]
+    }
   }
 
   return answer;
